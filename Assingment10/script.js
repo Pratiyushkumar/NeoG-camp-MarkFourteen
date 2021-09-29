ad9804b0790187cd65accd17d54ea732f688d39a
Pratiyush <pratiyush.inv@gmail.com>
2021-09-29 11:48:36 +0530

    Mark 14 Assignment

---
+++ Assingment10/script.js
@@ -0,0 +1,42 @@
+const intialPrice = document.querySelector("#intial-price");
+const stocksQuantity = document.querySelector("#number-of-stocks");
+const currentPrice = document.querySelector("#curent-price");
+const btnClicked = document.querySelector("#btn-output");
+const output = document.querySelector(".output");
+
+btnClicked.addEventListener("click", () => {
+  let initial = intialPrice.value;
+  let stocks = stocksQuantity.value;
+  let current = currentPrice.value;
+  getProfitLossOfStocks(initial, stocks, current);
+});
+
+function getProfitLossOfStocks(initial, stocks, current) {
+  if (initial !== "" || stocks !== "" || current !== "") {
+    if (initial > current) {
+      let loss = (initial - current) * stocks;
+      let lossPercentage = (loss / initial) * 100;
+      output.innerText = `Oops! you are in loss ${loss} by ${lossPercentage.toPrecision(
+        4
+      )}%`;
+      output.style.color = "red";
+
+      console.log(loss, lossPercentage);
+    } else if (current > initial) {
+      let gain = (current - initial) * stocks;
+      let gainPercentage = (gain / initial) * 100;
+      output.innerText = `yayy! you are in gain ${gain} by ${gainPercentage.toPrecision(
+        4
+      )}%`;
+      output.style.color = "green";
+      console.log(gain, gainPercentage);
+    } else {
+      output.innerText = "No Pain No Gain";
+      output.style.color = "orange";
+      console.log("No pain No Gain");
+    }
+  } else {
+    output.innerText = "Please enter the Value";
+    output.style.color = "red";
+  }
+}
